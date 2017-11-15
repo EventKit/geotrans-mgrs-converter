111effb793cb6f85fe6d01e97822f3e8a8c3d9db
gibranparvez <32872347+gibranparvez@users.noreply.github.com>
2017-11-15 11:58:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/geotrans3.7/CCS/linux_64
 export LD_LIBRARY_PATH
-sudo dnf install -y libstdc++.i686
+# sudo dnf install -y libstdc++.i686
 export MSPCCS_DATA=$(pwd)/geotrans3.7/data
