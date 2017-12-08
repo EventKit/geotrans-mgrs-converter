8baa5dd36a9bc8a259f9df66469d794e37938428
Gibran Parvez <gibran.parvez@rgi-corp.com>
2017-12-08 08:34:57 -0500

    Update scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=$(pwd)/geotrans3.7/CCS/linux_64
+export LD_LIBRARY_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/geotrans3.7/CCS/linux_64
 #sudo dnf install -y libstdc++.i686
-export MSPCCS_DATA=$(pwd)/geotrans3.7/data
+export MSPCCS_DATA=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/geotrans3.7/data
