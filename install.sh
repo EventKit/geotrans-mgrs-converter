8e30043e058eb020f7a9516ce71f52a2bd182e1d
Gibran Parvez <gibran.parvez@rgi-corp.com>
2017-12-08 09:39:03 -0500

    Bad substitution

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-LD_LIBRARY_PATH=$( cd "$( dirname "${$0[0]}" )" && pwd )/geotrans3.7/CCS/linux_64
+LD_LIBRARY_PATH=$( cd "$( dirname "${$0}" )" && pwd )/geotrans3.7/CCS/linux_64
 export LD_LIBRARY_PATH
 #sudo dnf install -y libstdc++.i686
-MSPCCS_DATA=$( cd "$( dirname "${$0[0]}" )" && pwd )/geotrans3.7/data
+MSPCCS_DATA=$( cd "$( dirname "${$0}" )" && pwd )/geotrans3.7/data
 export MSPCCS_DATA
