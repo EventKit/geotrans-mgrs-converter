6e8edae87444a9dd5da26ef643f91b1f1a620771
Gibran Parvez <gibran.parvez@rgi-corp.com>
2018-01-02 08:29:53 -0500

    Exit 0 on tar

--- download_geotrans.sh
+++ download_geotrans.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [ -e master.tgz ]
 then
-    tar -xzkf master.tgz
+    tar -xzkf master.tgz || :
 else
-    wget 'http://earth-info.nga.mil/GandG/geotrans/geotrans3.7/master.tgz' && tar -xzkf master.tgz || :
+    wget 'http://earth-info.nga.mil/GandG/geotrans/geotrans3.7/master.tgz'; tar -xzkf master.tgz || :
+
 fi
